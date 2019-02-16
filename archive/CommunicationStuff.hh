e6d673329ff863c63cefa4751eff66b724e2ab68
joshualarbi <jkarbi3@uwo.ca>
2019-02-16 11:54:10 -0500

    Archiving old (non-interrupt) code

---
+++ archive/CommunicationStuff.hh
@@ -0,0 +1,123 @@
+// CommunicationStuff.hh
+
+#ifndef COMMUNICATION_STUFF
+#define COMMUNICATION_STUFF
+
+#include "Arduino.h"
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "Electrical.hh"
+#include "ReadSensors.hh"
+
+// INFO FOR LOCAL ROUTER
+char* ssid = "WE MARS Rover";
+const char* password = "westillfirst";
+
+// COMMUNICATION CONSTANTS
+AsyncWebServer server(80);
+IPAddress staticIP(192,168,1,16);
+IPAddress gateway(10,10,10,1);
+IPAddress subnet(255,255,255,0);
+
+int motorShutdown = 0;
+
+// POST PARAMETERS
+const char* motor1 = "MOTOR1";
+const char* motor2 = "MOTOR2";
+
+//HTTP GET PARAMS
+const String motorParams[] = {"left-side", "right-side"};
+
+void inline connectToWiFi()
+{
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    // ensure our IP is 10.10.10.10
+    WiFi.config(staticIP, gateway, subnet);
+    
+    delay(100);
+  
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+  
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+    }
+
+    Serial.println("CONNECTED TO " + String(ssid));
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.macAddress());
+}
+
+void inline setupESPServer()
+{    
+  /**
+   * HTTP callback with paramaters
+   * Will be expecting <IP>/?left-side=#&right-side=#
+   * Thanks to:
+   * https://techtutorialsx.com/2017/12/17/esp32-arduino-http-server-getting-query-parameters/
+   */
+   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      
+      int numParams = request->params();
+      String left, right;
+      // Serial.println("PARAM CALLBACK");
+      for ( int i = 0; i < numParams; i++ ) {
+        AsyncWebParameter* p = request->getParam(i);
+        String name = p->name();
+        // Serial.println("Param " + name);
+        if (name == motorParams[0]) {
+            left = p->value();
+        } else if (name == motorParams[1]) {
+            right = p->value();
+        }
+      }
+       
+       // if all went well, we now have the two power percentage values
+       // use our Electrical API
+      //  Serial.print("Calling move motors with params " + left);
+       // Serial.println(" " + right);
+       moveMotors(left.toInt(), right.toInt());
+       
+       
+       // send response back with motor current vals
+       // and rotary encoder positions in JSON
+       // are both size 6 arrays
+       int* currentSensors = getCurrentValues();
+       int* posVals = getRotaryPositions();
+       String response = "{" 
+                + "\"Current-Left-0\":\"" + String(currentSensors[0]) 
+                + "\",\"Current-Left-1\":\"" + String(currentSensors[1])
+                + "\",\"Current-Left-2\":\"" + String(currentSensors[2])
+                + "\",\"Current-Right-0\":\"" + String(currentSensors[3])
+                + "\",\"Current-Right-1\":\"" + String(currentSensors[4])
+                + "\",\"Current-Right-2\":\"" + String(currentSensors[5])
+                + "\",\"Pos-Left-0\":\"" + String(posVals[0])
+                + "\",\"Pos-Left-1\":\"" + String(posVals[1])
+                + "\",\"Pos-Left-2\":\"" + String(posVals[2])
+                + "\",\"Pos-Right-0\":\"" + String(posVals[3])
+                + "\",\"Pos-Right-1\":\"" + String(posVals[4])
+                + "\",\"Pos-Right-2\":\"" + String(posVals[5]) 
+                + "\"}";
+        request->send(200, "text/plain", response);
+       
+       // send success
+       // request->send(200, "text/plain", "SUCCESS");
+   });
+
+  /* Test callback -> test connection without affecting motors */
+  server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){ 
+       // send success
+       request->send(200, "text/plain", "Hello!");
+   });
+   
+  server.begin();
+}
+
+#endif
---
+++ archive/src.ino
@@ -0,0 +1,69 @@
+/**
+ * src.ino -> main setup and loop
+ * 
+ * Thank you to:
+ * me-no-dev:   https://github.com/me-no-dev/ESPAsyncWebServer
+ */
+ 
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "CommunicationStuff.hh"
+#include "Electrical.hh"
+#include "esp32-hal-ledc.h"
+
+
+/**
+ * ESP-32 Pinout:
+ * See drive_code/images
+ */
+const int LEFT_DRIVE_CHANNELS[] = {1, 2, 3};
+const int RIGHT_DRIVE_CHANNELS[] = {4, 5, 6};
+
+// on left hand side of board
+const int LEFT_DRIVE_PINS[] = {32, 33, 25};
+
+// on right hand side of board
+const int RIGHT_DRIVE_PINS[] = {4, 2, 15};
+
+const int NUM_MOTORS_PER_SIDE = 3;
+
+// trying to set WiFi server to run on other core
+TaskHandle_t Task1;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // setup electrical stuff
+  for (int i = 0; i < NUM_MOTORS_PER_SIDE; i++) {
+    setup(LEFT_DRIVE_PINS[i], LEFT_DRIVE_CHANNELS[i]);
+    setup(RIGHT_DRIVE_PINS[i], RIGHT_DRIVE_CHANNELS[i]);
+
+    setDriveChannel(i, LEFT_DRIVE_CHANNELS[i]);
+    setDriveChannel(i+3, RIGHT_DRIVE_CHANNELS[i]);
+  }
+
+  // setup networking stuff
+  // TEST with router 
+  // Ethernet
+  // Manually
+  // 192.168.1.13
+  // 255.255.0.0
+  // 192.168.0.50
+  connectToWiFi();
+
+
+
+  xTaskCreatePinnedToCore(
+             &setupESPServer, /* Task function. */
+             "ServerTask",   /* name of task. */
+             10000,     /* Stack size of task */
+             NULL,      /* parameter of the task */
+             0,         /* priority of the task - set to 0 since this should override encoder interrupts */
+             &Task1,    /* Task handle to keep track of created task */
+             0);  
+
+   /// setupESPServer();
+}
+
+void loop() { /* Intentionally empty. Required for Asynchronous Web Server. */ }
